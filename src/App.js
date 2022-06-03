c6cf2d848972c433c729c23bbbcb138a448a2d47
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-04 02:11:03 +0600

    added changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import "./App.scss";
+
 
 function App() {
   return (
     <div className="App">
+      <div className="wrapper">
+        <nav className="navbar">
+          <div className="img__container">
+            <img src="https://www.farmersalmanac.com/wp-content/uploads/2020/11/sunrise.jpg" alt="#"/> 
+          </div>
+        </nav>
+        <header className="header">
+          <div className="img__container">
+            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sunrise-quotes-21-1586892331.jpg" alt="#"/>
+          </div>
+        </header>
+        <aside className="aside">
+          <ul>
+            <li>
+              hello
+            </li>
+            <li>
+              my friend
+            </li>
+            <li>
+              how are you
+            </li>
+            <li>
+              who knows, who knows?
+            </li>
+          </ul>
+        </aside>
+
+        <main className="main">
+          <div className="img__container">
+            <img src="https://s1.1zoom.me/big0/766/Fields_Grasslands_Birds_471434.jpg" alt="#"/>
+          </div>
+
+          <p>
+            asdhflakdjfh;lajsdfkl
+            a fjaskldfhakl farmersalmanacf aklsdfh 'adf
+            jasdjfafsdfa'
+          </p>
+        </main>
+
+        <footer className="footer">
+          footer
+        </footer>
+      </div>
       
     </div>
   );
